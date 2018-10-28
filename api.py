e63dc945475ae851388fd2c943c24e7a309a2a2c
nana ogura <>
2018-10-28 15:02:07 +0900

    add print message and body

--- api.py
+++ api.py
@@ -30,7 +30,9 @@ import line
 def callback(body, response = None):
     print('おうむ返し')
     # handle API call
+    print(('body: {}'.format(body))
     message = line.get_message(body)
+    print('message: {}'.format(message))
     reply_token = line.get_reply_token(body)
     if message == None:
         response.__status = HTTP_400
