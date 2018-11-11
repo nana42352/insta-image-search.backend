24cbdc02d159fe4e56eac169dc72d4994a3a1ea6
nana ogura <>
2018-11-11 20:56:27 +0900

    add import os

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
+import os
 line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_TOKEN'])
 
 import hug
